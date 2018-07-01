de5bd9780991048dbbec31ac9d9a43f30e63d21c
DenisTmenov <denis.tmenov@gmail.com>
2018-07-01 13:39:57 +0300

    first homework is done

--- demo-challenge/src/main/java/com/udemy/buchalka/spring/challenge/IOCAppChallenge1.java
+++ demo-challenge/src/main/java/com/udemy/buchalka/spring/challenge/IOCAppChallenge1.java
@@ -1,11 +1,14 @@
 package com.udemy.buchalka.spring.challenge;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IOCAppChallenge1 {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-challenge.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-challenge.xml");
         City cityAdelaide = (City) ctx.getBean("cityAdelaide");
         cityAdelaide.sityName();
+
+        ((ClassPathXmlApplicationContext) ctx).close();
     }
 }
--- demo-challenge/src/main/java/com/udemy/buchalka/spring/challenge/IOCAppChallenge2.java
+++ demo-challenge/src/main/java/com/udemy/buchalka/spring/challenge/IOCAppChallenge2.java
@@ -1,12 +1,15 @@
 package com.udemy.buchalka.spring.challenge;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class IOCAppChallenge2 {
 
     public static void main(String[] args) {
-        FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("beans-challenge.xml");
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("demo-challenge/beans-challenge2.xml");
         City cityAdelaide = (City) ctx.getBean("cityAdelaide");
         cityAdelaide.sityName();
+
+        ((FileSystemXmlApplicationContext) ctx).close();
     }
 }
